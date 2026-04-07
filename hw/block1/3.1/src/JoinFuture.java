de7083c781fac8a09c2604fec8abd3bc0fd8757c
dRamee <maksim.yolckin@yandex.com>
2026-04-07 21:31:28 +0700

    Task 3.1 is done

---
+++ hw/block1/3.1/src/JoinFuture.java
@@ -0,0 +1,60 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadFactory;
+
+class JoinFuture<V> {
+
+    Thread thread;
+    V result;
+    Throwable exception;
+
+    /**
+     * Waits for completion and returns result or throws ExecutionException
+     */
+    public V get() throws ExecutionException {
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new ExecutionException(e);
+        }
+
+        if (exception != null) {
+            throw new ExecutionException(exception);
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns true if thread finished (normally or with exception)
+     */
+    public boolean isDone() {
+        return !thread.isAlive();
+    }
+}
+
+class ThreadPerTaskExecutorService {
+
+    private final ThreadFactory factory;
+
+    public ThreadPerTaskExecutorService(ThreadFactory f) {
+        this.factory = f;
+    }
+
+    public <T> JoinFuture<T> submit(Callable<T> task) {
+        JoinFuture<T> future = new JoinFuture<>();
+
+        Thread t = factory.newThread(() -> {
+            try {
+                future.result = task.call();
+            } catch (Throwable e) {
+                future.exception = e;
+            }
+        });
+
+        future.thread = t;
+        t.start();
+
+        return future;
+    }
+}
---
+++ hw/block1/3.1/test/JoinFutureTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.ThreadFactory;
+
+class JoinFutureTest {
+
+    ThreadFactory factory = Thread::new;
+
+    @Test
+    void testSimpleResult() throws Exception {
+        var executor = new ThreadPerTaskExecutorService(factory);
+
+        var f = executor.submit(() -> 42);
+
+        assertEquals(42, f.get());
+    }
+
+    @Test
+    void testExceptionPropagation() {
+        var executor = new ThreadPerTaskExecutorService(factory);
+
+        var f = executor.submit(() -> {
+            throw new RuntimeException("fail");
+        });
+
+        assertThrows(Exception.class, f::get);
+    }
+
+    @Test
+    void testIsDoneBeforeGet() throws Exception {
+        var executor = new ThreadPerTaskExecutorService(factory);
+
+        var f = executor.submit(() -> 1);
+
+        f.get();
+
+        assertTrue(f.isDone());
+    }
+
+    @Test
+    void testParallelExecution() throws Exception {
+        var executor = new ThreadPerTaskExecutorService(factory);
+
+        var f1 = executor.submit(() -> {
+            Thread.sleep(100);
+            return 1;
+        });
+
+        var f2 = executor.submit(() -> 2);
+
+        assertNotSame(f1.thread, f2.thread);
+        assertEquals(2, f2.get());
+        assertEquals(1, f1.get());
+    }
+
+    @Test
+    void testMultipleTasks() throws Exception {
+        var executor = new ThreadPerTaskExecutorService(factory);
+
+        var f1 = executor.submit(() -> 10);
+        var f2 = executor.submit(() -> 20);
+
+        assertEquals(10, f1.get());
+        assertEquals(20, f2.get());
+    }
+}
